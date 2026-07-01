38355c44a3c85cc52cb861aeaa0d8eac90fb6f2a
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2026-07-02 01:09:32 +0530

    Auto-sync from frontend repo

--- internal/models/search.go
+++ internal/models/search.go
@@ -74,7 +74,8 @@ type RouteStop struct {
 type TripResult struct {
 	TripID           uuid.UUID `json:"trip_id" db:"trip_id"`
 	RouteName        string    `json:"route_name" db:"route_name"`
-	RouteNumber      *string   `json:"route_number,omitempty" db:"route_number"`
+	RouteNumber      *string   `json:"route_number" db:"route_number"`
+	ScheduleName     *string   `json:"schedule_name,omitempty" db:"schedule_name"`
 	BusType          string    `json:"bus_type" db:"bus_type"`
 	DepartureTime    time.Time `json:"-" db:"departure_time"`
 	EstimatedArrival time.Time `json:"-" db:"estimated_arrival"`
